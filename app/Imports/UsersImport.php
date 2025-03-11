d2ed8dced36a644f6c39a0007cd954140fa83754
zainudin <zainudinarab@gmail.com>
2025-03-11 15:29:09 +0700

    dddd

---
+++ app/Imports/UsersImport.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\User;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithValidation;
+use Hash;
+
+class UsersImport implements ToModel, WithHeadingRow, WithValidation
+{
+    /**
+     * @param array $row
+     *
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
+    public function model(array $row)
+    {
+        return new User([
+            'name'     => $row['name'],
+            'email'    => $row['email'],
+            'password' => Hash::make($row['password']),
+        ]);
+    }
+
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required',
+            'password' => 'required|min:5',
+            'email' => 'required|email|unique:users'
+        ];
+    }
+}
